314f93abf6d648ae42ecf41abbdd4196f1b0e282
subham kumar mal <subhammal53@gmail.com>
2021-08-26 14:06:24 +0530

    second commit

--- shopapp/static/shopapp/js/addtocart.js
+++ shopapp/static/shopapp/js/addtocart.js
@@ -1,5 +1,8 @@
+
+
 $(document).ready(function (){
     $('.btn-class-addtocart').on('click',function (){
+
         console.log(this.id)
         no=this.id.toString()
         name=document.getElementById(`sn${no}`).innerHTML
@@ -16,6 +19,10 @@ $(document).ready(function (){
             cart={}
             cart[no]={"name":name,'price':price,'count':1}
             sessionStorage.setItem('cart',JSON.stringify(cart))
+            buttonupdate(this.id)
+            cartupdata()
+
+
 
         }
         else{
@@ -23,16 +30,61 @@ $(document).ready(function (){
                  cart=JSON.parse(sessionStorage.getItem('cart'))
             cart[no]={'name':name,'price':price,'count':1}
             sessionStorage.setItem('cart',JSON.stringify(cart))
+              buttonupdate(this.id)
+              cartupdata()
           }
           else{
               cont=cart[no].count+1
             cart=JSON.parse(sessionStorage.getItem('cart'))
             cart[no]={'name':name,'price':price,'count':cont}
             sessionStorage.setItem('cart',JSON.stringify(cart))
+              cartupdata()
           }
 
         }
 
+
+            $('.plush').on('click',function (){
+    // console.log(this.id)
+    pid=this.id.toString()[1]
+    cart=JSON.parse(sessionStorage.getItem('cart'))
+    plushupdate(pid)
+    document.getElementById(`c${pid}`).innerHTML=cart[pid].count
+       })
+
     })
+
+
+
+
 })
+
+buttonupdate=(id)=>{
+    // console.log("buton",id)
+    cart=JSON.parse(sessionStorage.getItem('cart'))
+    document.getElementById(`span${id}`).innerHTML=`<button id="p${id}" class="plush" >+</button><span id="c${id}">${cart[id].count}</span><button id="m${id}" class="munish" >-</button>`
+}
+
+
+cartupdata=()=>{
+    console.log('clall')
+    total=0
+    cart=JSON.parse(sessionStorage.getItem('cart'))
+    // console.log(Object.keys(cart).length) to get the length of a dictionery in terms of keys
+    for( var i in cart){
+        total=total+cart[i].count
+    }
+    document.getElementById('cart-no').innerHTML=total.toString()
+
+
+}
+
+plushupdate=(id)=>{
+    cart=JSON.parse(sessionStorage.getItem('cart'))
+    cart[id].count=(cart[id].count)+1
+    sessionStorage.setItem('cart',JSON.stringify(cart))
+    // cartupdata()
+}
+
+
 
